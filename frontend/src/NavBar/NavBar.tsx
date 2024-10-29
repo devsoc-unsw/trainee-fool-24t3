53fdc6a96e5b27dde1512dd9560a71f01655cf66
Vincent Tannos <tannos.vincent@gmail.com>
2024-10-29 13:25:18 +1100

    🧭 Make Basic Navigation Bar Component

---
+++ frontend/src/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+import classes from './NavBar.module.css';
+import pyramidIcon from '../assets/pyramidIcon.svg';
+
+export type NavBarProps = {
+
+}
+
+function NavBar(props: NavBarProps) {
+  return (
+    <div className={classes.container}>
+      <div className={classes.pyramids}>
+        <img src={pyramidIcon} className={classes.logo} ></img>
+        <h1 className={classes.title}>Pyramids</h1>
+      </div>
+      <div className={classes.pages}>
+        <h1>Timeline</h1>
+        <h1>Calendar</h1>
+        <h1>About</h1>
+      </div>
+      <div className={classes.profile}></div>
+    </div>
+  );
+}
+
+export default NavBar;
