880c5163aeb5774f95485046034ae033fdd40ddd
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-10-31 16:44:28 +1100

    ✉ add basic boilerplate for discord

--- frontend/src/Society/Society.tsx
+++ frontend/src/Society/Society.tsx
@@ -1,27 +1,31 @@
 import classes from './Society.module.css';
 
 type SocietyProps = {
-    name: string;
-    image: string;
-    //These parameters can be passed as props otherwise some fault value maybe(?)
-    backgroundPositionY?: string;
-    color?:string
+  name: string;
+  image: string;
+  //These parameters can be passed as props otherwise some fault value maybe(?)
+  backgroundPositionY?: string;
+  color?: string;
 };
 
 function Society(props: SocietyProps) {
   return (
     <div className={classes.container}>
-        <div
+      <div
         className={classes.picture}
         style={{
-        backgroundImage: `url(${props.image})`,
-        backgroundPositionY: props.backgroundPositionY,
-        }}/>
-        <div className={classes.socNameDiv} style={{backgroundColor: props.color}}>
-            <h1 className={classes.socName}>{props.name}</h1>
-        </div>
+          backgroundImage: `url(${props.image})`,
+          backgroundPositionY: props.backgroundPositionY,
+        }}
+      />
+      <div
+        className={classes.socNameDiv}
+        style={{ backgroundColor: props.color ? props.color : 'red' }}
+      >
+        <h1 className={classes.socName}>{props.name}</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Society
+export default Society;
