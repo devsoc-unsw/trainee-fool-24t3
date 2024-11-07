4ea0a7e027fccf4eeb6bb307efe5ba3c5e8f55e0
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2024-11-07 15:56:11 +1100

    Preliminary register function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,61 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import session from "express-session";
+import cors from "cors";
+import { LoginBody, TypedRequest } from "./requestTypes";
+import bcrypt from "bcrypt";
+import { LoginErrors } from "./interfaces";
+import { PrismaClient, Prisma, UserType, User } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
 const SERVER_PORT = 5180;
-
 
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (req: Request, res: Response) => {
-    console.log('Hello, TypeScript with Express :)))!');
-    res.send('Hello, TypeScript with Express :)))!');
+app.get("/", (req: Request, res: Response) => {
+  console.log("Hello, TypeScript with Express :)))!");
+  res.send("Hello, TypeScript with Express :)))!");
 });
 
+app.post("/register", async (req: TypedRequest<LoginBody>, res: Response) => {
+  const { username, email, password, userType } = req.body;
+
+  // check database for existing user with same username
+  const errorCheck: LoginErrors = {
+    matchingCredentials: true,
+  };
+
+  const results = await prisma.user.findFirst({
+    where: {
+      OR: [{ username: username }, { email: email }],
+    },
+  });
+
+  if (results) {
+    errorCheck.matchingCredentials = true;
+    return res.status(400).json(errorCheck);
+  }
+
+  const saltRounds: number = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //add user prisma
+  await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+      salt,
+      userType,
+      dateJoined: new Date(),
+      profilePicture: null,
+    },
+  });
+
+  return res.status(201).send("User Successfully Registered");
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server running on port http://localhost:${SERVER_PORT}`);
---
+++ backend/tests/register.test.ts
@@ -0,0 +1,11 @@
+// Import necessary modules
+import { PrismaClient } from '@prisma/client';
+import 'jest';
+// Mock Prisma Client
+
+// Create a new PrismaClient instance (mocked)
+const prisma = new PrismaClient();
+
+describe("Prisma Client Tests", () => {
+
+}
