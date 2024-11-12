0827eb56fd823c9bebf3421dc1c0335d42655ece
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2024-11-12 13:26:44 +1100

    🔒wrote preliminary login function and tests

--- backend/tests/login.test.ts
+++ backend/tests/login.test.ts
@@ -5,27 +5,6 @@ import request from "supertest";
 import app from "../src/index";
 
 describe("Tests", () => {
-  test("register test", async () => {
-    const { status, body } = await request(app).post("/auth/register").send({
-      username: "shinjisatoo",
-      password: "testpassword",
-      email: "longseason1996@gmail.com",
-      userType: "ATTENDEE",
-    });
-
-    const newUser = await prisma.user.findFirst({
-      where: {
-        email: "longseason1996@gmail.com",
-      },
-    });
-
-    expect(status).toBe(201);
-    expect(newUser).not.toBeNull();
-    expect(body.newUser).toStrictEqual({
-      username: "shinjisatoo",
-      id: newUser?.id,
-    });
-  });
 
   test("Email already exists", async () => {
     await request(app).post("/auth/register").send({
