6b08e790c587d94d86e6ff27c91c71e56b64fba9
gyumi <pokemonigt@gmail.com>
2024-11-19 02:24:12 +1100

    👷 making adjustments to try to fix cron job issues part 2

--- backend/prisma/migrations/20241118101736_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `expiryTime` to the `OTPToken` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "OTPToken" ADD COLUMN     "expiryTime" TIMESTAMP(3) NOT NULL;
--- backend/prisma/migrations/20241118102217_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "OtpToken" ALTER COLUMN "token" SET DATA TYPE TEXT;
