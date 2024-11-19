09cb80920f4ba0e57d59e58147b7170262569db7
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-11-19 14:11:36 +1100

    🧒 add further /user tests

--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -5,7 +5,7 @@ import request from 'supertest';
 import app from '../src/index';
 import { SanitisedUser } from '../src/interfaces';
 
-describe('Tests', () => {
+describe('/user endpoint', () => {
   test('login success, fetch /user', async () => {
     const { status, body } = await request(app).post('/auth/register').send({
       username: 'shinjisatoo',
@@ -48,58 +48,15 @@ describe('Tests', () => {
     expect(userBody.profilePicture == newUser.profilePicture);
   });
 
-  // test('Unauthorized User', async () => {
-  //   const { status, body } = await request(app).post('/auth/register').send({
-  //     username: 'shinjisatoo',
-  //     password: 'testpassword',
-  //     email: 'longseason1997@gmail.com',
-  //     userType: 'ATTENDEE',
-  //   });
+  test('fetch /user without session', async () => {
+    const response2 = await request(app).get('/user');
+    expect(response2.status).toBe(401);
+  });
 
-  //   const newUser = await prisma.user.findFirst({
-  //     where: {
-  //       id: body.newUser.id,
-  //     },
-  //   });
-  //   if (newUser == null) return;
-  //   expect(status).toBe(201);
-  //   expect(newUser).not.toBeNull();
-
-  //   const response2 = await request(app).get('/user').send({
-  //     userId: newUser.id,
-  //   });
-  //   expect(response2.status).toBe(401);
-  // });
-
-  // test('Wrong password', async () => {
-  //   const { status, body } = await request(app).post('/auth/register').send({
-  //     username: 'shinjisatoo',
-  //     password: 'testpassword',
-  //     email: 'longseason1997@gmail.com',
-  //     userType: 'ATTENDEE',
-  //   });
-
-  //   const response = await request(app).post('/auth/login').send({
-  //     username: body.newUser.username,
-  //     password: 'wrongpassword',
-  //   });
-
-  //   expect(response.status).toBe(400);
-  // });
-
-  // test('Nonexistent Username', async () => {
-  //   const { status, body } = await request(app).post('/auth/register').send({
-  //     username: 'shinjisatoo',
-  //     password: 'testpassword',
-  //     email: 'longseason1997@gmail.com',
-  //     userType: 'ATTENDEE',
-  //   });
-
-  //   const response = await request(app).post('/auth/login').send({
-  //     username: "Idon'texistlol",
-  //     password: 'testpassword',
-  //   });
-
-  //   expect(response.status).toBe(400);
-  // });
+  test('fetch /user with invalid session', async () => {
+    const response2 = await request(app)
+      .get('/user')
+      .set('Cookie', 'invalid_session');
+    expect(response2.status).toBe(401);
+  });
 });
