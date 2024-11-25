84e2338b35884aea221d09c4fa49865d5d9acbd4
Aaron Fang <z5600511@ad.unsw.edu.au>
2024-11-26 01:55:01 +1100

    added computation for dates and weekdays for timeline, both pretend dates and actual dates once they get added, except for displaying events. also added in the frontend pretties so that the timeline looks as close to figma as possible, except its pink because i need it to be not white to see it.

--- frontend/src/timeline.tsx
+++ frontend/src/timeline.tsx
@@ -7,6 +7,20 @@ import {
 
 const startDate = new Date(2024, 9, 28);
 const endDate = new Date(2024, 10, 1);
+const days = Array.from({ length: 7 }, (e, i) => {
+
+  e = 0; //straight up this line is just here to get rid of "value declared but not read" warning. 
+  // it was bugging me
+
+  const date: Date = new Date(startDate.valueOf());
+
+  date.setDate(startDate.getDate() + i);
+
+  return {
+    date,
+    events: []
+  };
+});
 
 function Day(dayProps: DayInfo) {
 
@@ -24,24 +38,32 @@ function Timeline() {
   const currentTimeline: TimelineInfo = {
     startDate,
     endDate,
-    days: []
+    days
   };
-  // Replace object above with function to retrieve timeline.
 
+  // Replace object above with function to retrieve timeline.
+  const getDate = function (day: DayInfo, options: { }): string {
+    return day.date.toLocaleDateString('en-GB', options);
+  }
   return (
     <div className={classes.container}>
       {
         currentTimeline.days.map((day: DayInfo) => Day(day))
       }
-      <p>This is the preliminary version of the timeline.</p>
-      <p>It currently has no data because I couldn't be bothered,</p>
-      <p>So the text here is paragraph tags placed manually</p>
-      <p>To show that the div appears and exists at least</p>
-    </div>
+     <div className={classes.timelineBar}>
+       <div className={classes.arrowContainer}><p className={classes.arrowTextLeft}>&larr;</p></div>
+       <div className={classes.daysContainer}>
+       {currentTimeline.days.map((day) => (
+        <div className={classes.dayContainerBox}>
+          <p className={classes.weekdayText}>{ getDate(day, { weekday: 'long' }) }</p>
+          <p className={classes.dateText}>{ getDate(day, { day: 'numeric', month: 'numeric' }) }</p>
+        </div>
+       ))} 
+       </div>
+       <div className={classes.arrowContainer}><p className={classes.arrowTextRight}>&rarr;</p></div>
+     </div>
+   </div>
   );
 }
-
-
-
 
 export default Timeline;
