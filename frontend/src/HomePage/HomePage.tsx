7bc8ccf06ee1c66f47105f600eddaf7bbfe09329
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-12-03 13:11:06 +1100

    🛳 add basic landing page

--- frontend/src/HomePage/HomePage.tsx
+++ frontend/src/HomePage/HomePage.tsx
@@ -1,3 +1,17 @@
+import classes from "./HomePage.module.css";
+import logo from "../assets/pyramidIcon.svg";
+
 export default function HomePage() {
-  return <h1>Hi</h1>;
+  return (
+    <div className={classes.homePage}>
+      <header className={classes.header}>
+        <img className={classes.logo} src={logo} alt="Pyramids logo" />
+        <h1>Pyramids</h1>
+      </header>
+      <main className={classes.main}>
+        <h2>Welcome!</h2>
+        <p>Click the icon in the bottom right to log in.</p>
+      </main>
+    </div>
+  );
 }
