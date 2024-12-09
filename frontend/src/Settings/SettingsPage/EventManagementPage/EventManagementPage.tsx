0ae35feab424d6d998caa29a22a70b0932c04bb9
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-12-10 01:25:33 +1100

    👶 begin work on profile page

--- frontend/src/Settings/SettingsPage/EventManagementPage/EventManagementPage.tsx
+++ frontend/src/Settings/SettingsPage/EventManagementPage/EventManagementPage.tsx
@@ -14,7 +14,7 @@ export function EventManagementPage() {
         />,
       ]}
     >
-      <p>Hello</p>
+      <p>TODO</p>
     </SettingsPage>
   );
 }
--- frontend/src/Settings/SettingsPage/ProfilePage/ProfilePage.tsx
+++ frontend/src/Settings/SettingsPage/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,16 @@
 import { SettingsPage } from '../SettingsPage';
+import classes from './ProfilePage.module.css';
 
 export function ProfilePage() {
   return (
     <SettingsPage title="Profile" headerButtons={[]}>
-      <div>Hello</div>
+      <section className={classes.heading}>
+        <img
+          src="https://i.redd.it/white-pharaoh-in-school-textbook-v0-fgr8oliazlkd1.jpg?width=225&format=pjpg&auto=webp&s=04dc4c2c8a0170c4e161091673352cd966591475"
+          alt="Profile picture"
+        />
+        <h2>John Pharoah</h2>
+      </section>
     </SettingsPage>
   );
 }
