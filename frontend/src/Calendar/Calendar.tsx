1e850b546dac476af46f9e0bdd17f0da3d159eb8
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2024-12-11 00:25:54 +0800

    🏄‍♂️Created route which gets all events within a month

--- frontend/src/Calendar/Calendar.tsx
+++ frontend/src/Calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import { format, startOfMonth, endOfMonth, eachDayOfInterval, getDay, addMonths, subMonths, differenceInDays, getDaysInMonth } from 'date-fns'
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline"
 import classes from './Calendar.module.css'
@@ -29,38 +29,39 @@ function Calendar() {
     const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     const startDayIndex = mapDayIndex(getDay(firstDay));
     const dayBuffer = (42-(getDaysInMonth(currentDate)+startDayIndex));
-    console.log(differenceInDays(firstDay, lastDay)+2)
-  return (
-    <div className={classes.calendarPage}>
-        <div className={classes.calendarElem}>
-            <div className={classes.calendarHeader}>
-                <h1 className={classes.monthYearName}>{format(currentDate, "MMMM yyyy")}</h1>
-                <div className={classes.calendarNavButtons}>
-                    <ArrowLeftIcon className={classes.navButton} onClick={() => subMonth(currentDate)}/>
-                    <ArrowRightIcon className={classes.navButton} onClick={() => addMonth(currentDate)}/>
+    const [eventsInMonth, setEventsInMonth] = useState([]);
+
+    return (
+        <div className={classes.calendarPage}>
+            <div className={classes.calendarElem}>
+                <div className={classes.calendarHeader}>
+                    <h1 className={classes.monthYearName}>{format(currentDate, "MMMM yyyy")}</h1>
+                    <div className={classes.calendarNavButtons}>
+                        <ArrowLeftIcon className={classes.navButton} onClick={() => subMonth(currentDate)}/>
+                        <ArrowRightIcon className={classes.navButton} onClick={() => addMonth(currentDate)}/>
+                    </div>
+                </div>
+                <div className={classes.weekdayRow}>
+                    { days.map((day) => {
+                        return <CalendarWeekdayCell day={day}/>
+                    })}
+                </div>
+                <div className={classes.days}>
+                    {Array.from({length:startDayIndex}).map((_, index) => {
+                        return <CalendarCell/>
+                    })}
+
+                    { daysOfMonth.map((day) => {
+                        return <CalendarCell date={format(day, 'dd/MM')}/>
+                    })}
+
+                    {Array.from({length:dayBuffer}).map((_, index) => {
+                        return <CalendarCell/>
+                    })}
                 </div>
             </div>
-            <div className={classes.weekdayRow}>
-                { days.map((day) => {
-                    return <CalendarWeekdayCell day={day}/>
-                })}
-            </div>
-            <div className={classes.days}>
-                {Array.from({length:startDayIndex}).map((_, index) => {
-                    return <CalendarCell/>
-                })}
-
-                { daysOfMonth.map((day) => {
-                    return <CalendarCell date={format(day, 'dd/MM')}/>
-                })}
-
-                {Array.from({length:dayBuffer}).map((_, index) => {
-                    return <CalendarCell/>
-                })}
-            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Calendar
