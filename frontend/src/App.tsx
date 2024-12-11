d6ef100e8801c29ff64860830e6824109d3fe425
Aaron Fang <z5600511@ad.unsw.edu.au>
2024-12-11 19:36:29 +1100

    adding unfinished mobile timeline page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import bbq from "./assets/bbq.png";
 import NavBar from "./NavBar/NavBar";
 import Button, { ButtonOptions } from "./Button/Button";
 import CreateEvent from "./CreateEvent/CreateEvent";
-import Timeline from './timeline';
+import { TimelinePage, PCTimelinePage, MobileTimelinePageFrame } from './timeline';
 
 function App() {
   return (
@@ -33,7 +33,10 @@ function App() {
       <Button type={ButtonOptions.Bookmark}></Button>
       <Button type={ButtonOptions.String}>Log in</Button>
       <CreateEvent></CreateEvent>
-      <Timeline></Timeline>
+      <TimelinePage></TimelinePage>
+      <PCTimelinePage></PCTimelinePage>
+      <MobileTimelinePageFrame></MobileTimelinePageFrame>
+
       <NavBar profileImage="https://i.redd.it/white-pharaoh-in-school-textbook-v0-fgr8oliazlkd1.jpg?width=225&format=pjpg&auto=webp&s=04dc4c2c8a0170c4e161091673352cd966591475"></NavBar>
     </>
   );
--- frontend/src/timeline.tsx
+++ frontend/src/timeline.tsx
@@ -5,11 +5,18 @@ import {
   TimelineInfo
 } from '../../backend/src/interfaces';
 
+const dayMonthOption = { day: 'numeric', month: 'numeric' };
+const weekdayOption = { weekday: 'long' };
+
+const getDate = function (date: Date, options: { }): string {
+  return date.toLocaleDateString('en-GB', options);
+};
+
 const startDate = new Date(2024, 9, 28);
-const endDate = new Date(2024, 10, 1);
+const endDate = new Date(2024, 10, 3);
 const days = Array.from({ length: 7 }, (e, i) => {
 
-  e = 0; //straight up this line is just here to get rid of "value declared but not read" warning. 
+  e = 50; //straight up this line is just here to get rid of "value declared but not read" warning. 
   // it was bugging me
 
   const date: Date = new Date(startDate.valueOf());
@@ -33,8 +40,43 @@ function Day(dayProps: DayInfo) {
   );
 }
 
-function Timeline() {
+function TimelineTitle(TimelineTitleProps: TimelineInfo) {
+  const { startDate, endDate } = TimelineTitleProps;
+
+  return (
+    <div className={classes.timelineTitle}>
+      <p className={classes.weekTitle}>{'Week 8' /*I have no clue what to put here */}</p>
+      <p className={classes.dateRange}>{`${getDate(startDate, dayMonthOption)} - ${getDate(endDate, dayMonthOption)}`}</p>
+    </div>
+  );
+}
+
+function TimelineBar(TimelineProps: TimelineInfo) {
   
+  return (
+    <div className={classes.timelineBarContainer}>
+      <div className={classes.arrowContainer}><p className={classes.arrowText1}></p></div>
+      <div className={classes.daysContainer}>
+        { TimelineProps.days.map((day) => {
+          let weekdayString = getDate(day.date, weekdayOption);
+            return (
+            
+            <div className={classes.dayContainerBox}>
+              <p className={classes.weekdayInitial}>{ weekdayString[0] }</p>
+              <p className={classes.weekdayText}>{ weekdayString }</p>
+              <p className={classes.dateText}>{ getDate(day.date, dayMonthOption) }</p>
+            </div>
+            );
+          })
+        } 
+      </div>
+      <div className={classes.arrowContainer}><p className={classes.arrowText2}></p></div>
+    </div>
+  );
+}
+
+function TimelinePage() {
+
   const currentTimeline: TimelineInfo = {
     startDate,
     endDate,
@@ -42,28 +84,68 @@ function Timeline() {
   };
 
   // Replace object above with function to retrieve timeline.
-  const getDate = function (day: DayInfo, options: { }): string {
-    return day.date.toLocaleDateString('en-GB', options);
-  }
+
   return (
-    <div className={classes.container}>
-      {
-        currentTimeline.days.map((day: DayInfo) => Day(day))
-      }
-     <div className={classes.timelineBar}>
-       <div className={classes.arrowContainer}><p className={classes.arrowTextLeft}>&larr;</p></div>
-       <div className={classes.daysContainer}>
-       {currentTimeline.days.map((day) => (
-        <div className={classes.dayContainerBox}>
-          <p className={classes.weekdayText}>{ getDate(day, { weekday: 'long' }) }</p>
-          <p className={classes.dateText}>{ getDate(day, { day: 'numeric', month: 'numeric' }) }</p>
-        </div>
-       ))} 
-       </div>
-       <div className={classes.arrowContainer}><p className={classes.arrowTextRight}>&rarr;</p></div>
-     </div>
-   </div>
+    <div className={classes.timelinePageContainer}>
+      { TimelineTitle(currentTimeline) }
+
+      { currentTimeline.days.map((day: DayInfo) => Day(day)) }
+
+      { TimelineBar(currentTimeline) }
+    </div>
   );
 }
 
-export default Timeline;
+
+function PCTimelinePage() {
+
+  const currentTimeline: TimelineInfo = {
+    startDate,
+    endDate,
+    days
+  };
+
+  // Replace object above with function to retrieve timeline.
+
+  return (
+    <div className={classes.PCTimelineContainer}>
+      { TimelineTitle(currentTimeline) }
+
+      { currentTimeline.days.map((day: DayInfo) => Day(day)) }
+
+      { TimelineBar(currentTimeline) }
+    </div>
+  );
+}
+
+function MobileTimelinePage() {
+
+  const currentTimeline: TimelineInfo = {
+    startDate,
+    endDate,
+    days
+  };
+
+  // Replace object above with function to retrieve timeline.
+
+  return (
+    <div className={classes.mobileTimelineContainer}>
+      { TimelineTitle(currentTimeline) }
+
+      { currentTimeline.days.map((day: DayInfo) => Day(day)) }
+
+      { TimelineBar(currentTimeline) }
+    </div>
+  );
+}
+
+function MobileTimelinePageFrame() {
+
+  return (
+    <div className={classes.mobileTimelineFrameContainer}>
+      { MobileTimelinePage() }  
+    </div>
+  );
+}
+
+export { TimelinePage, PCTimelinePage, MobileTimelinePage, MobileTimelinePageFrame };
