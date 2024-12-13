23d327132712cdd45a2e192cb3b77b8c2b626e4c
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-12-14 02:10:56 +1100

    🔢 begin work on otp frontend (thanks ricky)

--- backend/src/routes/OTP/generateOTP.ts
+++ backend/src/routes/OTP/generateOTP.ts
@@ -3,17 +3,21 @@ import { getUserFromEmail } from './OTPToken';
 import { sendEmail } from './sendEmail';
 
 export const generateOTP = async (emailAddress: string) => {
-    const user = await getUserFromEmail(emailAddress);
-    
-    if(!user) {
-      throw new Error("User not found.");
-    }
-    
-    const rand = crypto.randomBytes(4).readUint32BE(0);
-    const sixDigits = (rand % 900000) + 100000;
-    const otpCode = sixDigits.toString(); 
-    
-    await sendEmail(emailAddress, user.username, otpCode);
+  const user = await getUserFromEmail(emailAddress);
 
-    return otpCode;
-}
+  if (!user) {
+    throw new Error("User doesn't exist!");
+  }
+
+  const rand = crypto.randomBytes(4).readUint32BE(0);
+  const sixDigits = (rand % 900000) + 100000;
+  const otpCode = sixDigits.toString();
+
+  try {
+    await sendEmail(emailAddress, user.username, otpCode);
+  } catch (error) {
+    throw new Error('Error sending OTP email');
+  }
+
+  return otpCode;
+};
--- frontend/src/errorHandler.ts
+++ frontend/src/errorHandler.ts
@@ -1,24 +1,62 @@
 export interface AuthError {
-    message: string;
-    usernameError: boolean;
-    passwordError: boolean;
-    emailError: boolean;
+  message: string;
+  usernameError: boolean;
+  passwordError: boolean;
+  emailError: boolean;
 }
 
 export function errorHandler(error: string): AuthError {
-    switch(error) {
-        case "Missing required fields":
-            return {message: error, usernameError: true, passwordError: true, emailError: true,}
-        case "User doesnt exist!":
-            return {message: error, usernameError: true, passwordError: false, emailError: false,}
-        case "Invalid password":
-            return {message: error, usernameError: false, passwordError: true, emailError: false,}
-        case "Account with same credentials already exists":
-            return {message: error, usernameError: true, passwordError: false, emailError: true,}
-        case "Error logging in":
-            return {message: "There was an internal server error while you were logging in, try again later.", usernameError: true, passwordError: true, emailError: true,}
-        default:
-            return {message: `An unexpected error occured: ${error}`, usernameError: true, passwordError: true, emailError: true,}
-    }
-
+  switch (error) {
+    case 'Missing required fields':
+      return {
+        message: error,
+        usernameError: true,
+        passwordError: true,
+        emailError: true,
+      };
+    case 'User doesnt exist!':
+      return {
+        message: error,
+        usernameError: true,
+        passwordError: false,
+        emailError: false,
+      };
+    case 'Invalid password':
+      return {
+        message: error,
+        usernameError: false,
+        passwordError: true,
+        emailError: false,
+      };
+    case 'Account with same credentials already exists':
+      return {
+        message: error,
+        usernameError: true,
+        passwordError: false,
+        emailError: true,
+      };
+    case 'Error logging in':
+      return {
+        message:
+          'There was an internal server error while you were logging in, please try again later.',
+        usernameError: true,
+        passwordError: true,
+        emailError: true,
+      };
+    case 'Error sending OTP email':
+      return {
+        message:
+          'There was an internal server error while sending the OTP email, please try again later.',
+        usernameError: true,
+        passwordError: true,
+        emailError: true,
+      };
+    default:
+      return {
+        message: `An unexpected error occured: ${error}`,
+        usernameError: true,
+        passwordError: true,
+        emailError: true,
+      };
+  }
 }
